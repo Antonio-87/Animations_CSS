35372ab530f6bde5eae7c3ad0547f73f35bac633
Antonio-87 <davydenko.87@list.ru>
2023-07-10 22:55:04 +0300

    prodaction

--- src/js/Popover.js
+++ src/js/Popover.js
@@ -14,36 +14,52 @@ export default class Popover {
     popover.textContent = this.#text;
     popover.style.cssText = `
             display; block;
+            box-sizing: border-box;
+            overflow-y: hidden;
             color: gray;
             font-size: 16px;
             line-height: 1.5;
             position: absolute;
             width: 500px;
+            height: 0;
+            transition: height 0.5s ease-in-out;
             left: 0px;
             top: ${this.#element.offsetHeight + 20}px;
             border: solid 1px gray;
             border-radius: 5px;
-            transform: scale(1, 1);
             z-index: 999;
     `;
     this.#element.append(popover);
-    this.#element.querySelector(".popover").classList.add("transition");
+    this.#popoverDom = popover;
+
+    setTimeout(() => {
+      popover.style.height = "98px";
+    }, 0);
   }
 
   onClick = (e) => {
     const target = e.target;
-    this.#popoverDom = document.querySelector(".popover");
-    if (!this.#popoverDom) {
-      this.#element.classList.add("focus");
-      this.#setPopover();
-    } else {
-      this.#element.classList.remove("focus");
-      this.#popoverDom.remove();
+    if (target.classList.contains("button-collapse")) {
+      if (!this.#popoverDom) {
+        this.#element.classList.add("focus");
+        this.#setPopover();
+      } else {
+        this.#element.classList.remove("focus");
+        this.#popoverDom.style.height = "0px";
+        setTimeout(() => {
+          this.#popoverDom.remove();
+          this.#popoverDom = null;
+        }, 500);
+      }
     }
 
-    if (!target) {
+    if (!target.classList.contains("button-collapse") && this.#popoverDom) {
       this.#element.classList.remove("focus");
-      this.#popoverDom.remove();
+      this.#popoverDom.style.height = "0px";
+      setTimeout(() => {
+        this.#popoverDom.remove();
+        this.#popoverDom = null;
+      }, 500);
     }
   };
 }
